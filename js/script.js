113eab464e8815ce49249683b498e282e800f08e
AhmedYMousa <A-Mousa@hotmail.com>
2017-11-20 13:54:31 +0200

    Finish about section Edit SmoothScroll Setup Portfolio CSS Grid

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
     $("body").css("animation", "bodyAnimation ease-in-out 1s 1");
     $("#navbar").on("click", "li >a", function () {
         $("html,body").animate({
-            scrollTop: $(this.hash).offset().top
+            scrollTop: $(this.hash).offset().top-70
         }, 1000);
+        console.log( $(this.hash).offset().top);
     });
 });
