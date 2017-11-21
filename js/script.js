c0c2be2f1f25985624bd191621690d259aa3cacc
AhmedYMousa <A-Mousa@hotmail.com>
2017-11-21 18:59:01 +0200

    Activate body animation,and this project is complete...

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    // $("body").css("animation", "bodyAnimation ease-in-out 1s 1");
+    $("body").css("animation", "bodyAnimation ease-in-out 1s 1");
     $("#navbar").on("click", "li > a", function (event) {
         $("html,body").animate({
             scrollTop: $(this.hash).offset().top - 70}, 1000);
